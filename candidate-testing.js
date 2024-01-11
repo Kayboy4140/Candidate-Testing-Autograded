714368976f42d4ed4959ec2b53f3ee251221bcf7
Your Name <you@example.com>
2024-01-11 15:48:10 -0500

    part 1: Minimum Viable Quiz completed

--- candidate-testing.js
+++ candidate-testing.js
@@ -18,12 +18,12 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-let candidateName = input.question("Enter your name");
+let candidateName = input.question("Enter your name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-question = "Who was the first American woman in space? ";
+console.log(question);
 candidateAnswer = input.question("What is your answer? ");
 }
 
@@ -31,9 +31,9 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 if (candidateAnswer === "Sally Ride") {
-  console.log("Correct!");
+  console.log("Your answer is correct!");
 } else {
-  console.log("Incorrect!")
+  console.log("Your answer is incorrect!")
 }
 
 
